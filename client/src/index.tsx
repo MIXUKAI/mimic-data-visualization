2bdfdb85dee5ae2700817848c00e4b2b35c373d0
xukai mi <xukai.mi@ceasia.com.cn>
2019-03-22 16:00:53 +0800

    chore: support ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root') as HTMLElement
 )
 
 // If you want your app to work offline and load faster, you can change
--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   paths: function(paths, env) {
-    paths.appIndexJs = path.resolve(__dirname, 'client/src/index.js')
+    paths.appIndexJs = path.resolve(__dirname, 'client/src/index.tsx')
     paths.appSrc = path.resolve(__dirname, 'client/src')
     return paths
   },
