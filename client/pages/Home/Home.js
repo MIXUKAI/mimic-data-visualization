a7925c967e5197ff113f1dde45bb70458715ce6e
mixukai <mixukai96@gmail.com>
2019-04-24 12:22:21 +0800

    refactor: home style

--- client/pages/Home/Home.js
+++ client/pages/Home/Home.js
@@ -2,36 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Head from './Head.component'
 import { Container, Header } from 'semantic-ui-react'
-import { Pie } from 'react-chartjs-2'
 
 import { getColors } from '../../util/colors'
-import { overview } from './Home.duck'
 import withCurrentView from '../../hoc/withCurrentView'
+import './Home.css'
 class Home extends Component {
-
-  componentDidMount() {
-    this.props.overview()
-  }
-
   render() {
     const { admission } = this.props
 
     const admissionData = { labels: [], datasets: [] }
     admissionData.labels = admission.map(ad => ad.admission_type)
-    admissionData.datasets.push({ data: admission.map(ad => ad.count), backgroundColor: getColors() })
+    admissionData.datasets.push({
+      data: admission.map(ad => ad.count),
+      backgroundColor: getColors(),
+    })
     return (
-      <div>
+      <div style={{ backgroundColor: '#f9f9f9'}}>
         <Head />
-        <Container>
-          {/* <div style={{ position: 'relative', width: 500, height: 400 }}> */}
-            <h3>Chart Samples</h3>
-            <Pie
-              height={100}
-              data={admissionData}
-            />
-          {/* </div> */}
-          <Header as="h2">Welcome!</Header>
-          <p>
+        <Container className="home-container">
+          <h2 className="home-container-h2">Welcome!</h2>
+          <p className="home-container-p">
             The MIMIC II Visualization tool is a web based tool that allows
             users to explore and compare some of the basic trends within the
             MIMIC II database. The information presented within the tool is not
@@ -44,7 +34,7 @@ class Home extends Component {
             incorporates exploration and comparison functionality. More
             information and instructions can be found on each respective page.
           </p>
-          <p>
+          <p className="home-container-p">
             Unfortunately this tool is still within its development and is still
             rather unstable at this time. While portions of it will display and
             render correctly, other times it will not. Should you be experience
@@ -54,22 +44,22 @@ class Home extends Component {
             notifications regarding JavaScript code encountering issues. Please
             select to continue the process, as it will eventually complete.
           </p>
-          <p>
+          <p className="home-container-p">
             The tool itself is based on a Linux, Apache, Postgres, PHP (LAPP)
             stack variant that pulls data from a server side backend to return a
             visual result. For more information on this you can take a look at a
             breakdown of how the tool works here.
           </p>
         </Container>
-        <Container>
-          <Header as="h2">About MIMIC III</Header>
-          <p>
+        <Container className="home-container">
+          <h2 className="home-container-h2">About MIMIC III</h2>
+          <p className="home-container-p">
             The MIMIC II database is a public intensive care database from Beth
             Israel Deaconess Medical Centre in Boston, MA, USA and is maintained
             by MIT. This tool displays aggregate patient data from version 2.6
             of MIMIC II.
           </p>
-          <p>
+          <p className="home-container-p">
             For more information about MIMIC II, please visit the official MIMIC
             II website at: http://physionet.org/mimic2
           </p>
@@ -86,9 +76,4 @@ const mapState = state => {
   }
 }
 
-export default withCurrentView(
-  connect(
-    mapState,
-    { overview }
-  )(Home)
-)
+export default withCurrentView(connect(mapState)(Home))
